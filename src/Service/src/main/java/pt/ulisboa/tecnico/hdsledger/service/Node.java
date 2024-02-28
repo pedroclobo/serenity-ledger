cdbec75e86d381abc767b53ffdbdf8c865e2251b
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-02-28 14:23:19 +0000

    feat,refactor: validate number of cli arguments and better error handling

--- src/Service/src/main/java/pt/ulisboa/tecnico/hdsledger/service/Node.java
+++ src/Service/src/main/java/pt/ulisboa/tecnico/hdsledger/service/Node.java
@@ -6,6 +6,8 @@ import pt.ulisboa.tecnico.hdsledger.communication.Link;
 import pt.ulisboa.tecnico.hdsledger.service.services.ClientService;
 import pt.ulisboa.tecnico.hdsledger.service.services.NodeService;
 import pt.ulisboa.tecnico.hdsledger.utilities.CustomLogger;
+import pt.ulisboa.tecnico.hdsledger.utilities.ErrorMessage;
+import pt.ulisboa.tecnico.hdsledger.utilities.HDSSException;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
 import java.text.MessageFormat;
@@ -21,39 +23,48 @@ public class Node {
 
   public static void main(String[] args) {
 
+    // Parse command line arguments
+    if (args.length != 2) {
+      System.err.println("Usage: java Node <nodeId> <nodesConfigPath>");
+      System.exit(1);
+    }
+    String id = args[0];
+    nodesConfigPath += args[1];
+
+    // Retrieve nodes and client configurations
+    ProcessConfig[] nodeConfigs = new ProcessConfigBuilder().fromFile(nodesConfigPath);
+    ProcessConfig[] clientConfigs = new ProcessConfigBuilder().fromFile(clientsConfigPath);
+
+    LOGGER.log(Level.INFO, MessageFormat.format(
+        "Node configuration: {0}\nClient Configuration: {1}", nodesConfigPath, clientsConfigPath));
+
+    // Retrieve the current node's config and the leader's config
+    ProcessConfig nodeConfig = Arrays.stream(nodeConfigs).filter(c -> c.getId().equals(id))
+        .findAny().orElseThrow(() -> new HDSSException(ErrorMessage.NoSuchNode));
+
+    ProcessConfig leaderConfig = Arrays.stream(nodeConfigs).filter(ProcessConfig::isLeader)
+        .findAny().orElseThrow(() -> new HDSSException(ErrorMessage.NoLeaderNode));
+
+    LOGGER.log(Level.INFO,
+        MessageFormat.format(
+            "Node with id {0} with node socket on <{1}:{2}> and client socket on <{3}:{4}> and leader={5}",
+            nodeConfig.getId(), nodeConfig.getHostname(), nodeConfig.getPort(),
+            nodeConfig.getHostname(), nodeConfig.getClientPort(), nodeConfig.isLeader()));
+
+    // Abstraction to send and receive messages
+    Link linkToNodes =
+        new Link(nodeConfig, nodeConfig.getPort(), nodeConfigs, ConsensusMessage.class);
+    Link linkToClients =
+        new Link(nodeConfig, nodeConfig.getClientPort(), clientConfigs, AppendMessage.class);
+
+    // Services that implement listen from UDPService
+    NodeService nodeService = new NodeService(linkToNodes, nodeConfig, leaderConfig, nodeConfigs);
+    ClientService clientService =
+        new ClientService(linkToClients, nodeConfig, leaderConfig, nodeConfigs, nodeService);
+
     try {
-      // Command line arguments
-      String id = args[0];
-      nodesConfigPath += args[1];
-
-      // Create configuration instances
-      ProcessConfig[] nodeConfigs = new ProcessConfigBuilder().fromFile(nodesConfigPath);
-      ProcessConfig[] clientConfigs = new ProcessConfigBuilder().fromFile(clientsConfigPath);
-      ProcessConfig leaderConfig =
-          Arrays.stream(nodeConfigs).filter(ProcessConfig::isLeader).findAny().get();
-      ProcessConfig nodeConfig =
-          Arrays.stream(nodeConfigs).filter(c -> c.getId().equals(id)).findAny().get();
-
-      LOGGER.log(Level.INFO,
-          MessageFormat.format(
-              "Node {0}:\nNode Socket: {1}:{2}\nClient Socket: {3}:{4}\nIs Leader? {5}",
-              nodeConfig.getId(), nodeConfig.getHostname(), nodeConfig.getPort(),
-              nodeConfig.getHostname(), nodeConfig.getClientPort(), nodeConfig.isLeader()));
-
-      // Abstraction to send and receive messages
-      Link linkToNodes =
-          new Link(nodeConfig, nodeConfig.getPort(), nodeConfigs, ConsensusMessage.class);
-      Link linkToClients =
-          new Link(nodeConfig, nodeConfig.getClientPort(), clientConfigs, AppendMessage.class);
-
-      // Services that implement listen from UDPService
-      NodeService nodeService = new NodeService(linkToNodes, nodeConfig, leaderConfig, nodeConfigs);
-      ClientService clientService =
-          new ClientService(linkToClients, nodeConfig, leaderConfig, nodeConfigs, nodeService);
-
       nodeService.listen();
       clientService.listen();
-
     } catch (Exception e) {
       e.printStackTrace();
     }
--- src/Utilities/src/main/java/pt/ulisboa/tecnico/hdsledger/utilities/ErrorMessage.java
+++ src/Utilities/src/main/java/pt/ulisboa/tecnico/hdsledger/utilities/ErrorMessage.java
@@ -4,8 +4,10 @@ public enum ErrorMessage {
   ConfigFileNotFound(
       "The configuration file is not available at the path supplied"), ConfigFileFormat(
           "The configuration file has wrong syntax"), NoSuchNode(
-              "Can't send a message to a non existing node"), SocketSendingError(
-                  "Error while sending message"), CannotOpenSocket("Error while opening socket");
+              "Can't send a message to a non existing node"), NoLeaderNode(
+                  "No leader node available"), SocketSendingError(
+                      "Error while sending message"), CannotOpenSocket(
+                          "Error while opening socket"),;
 
   private final String message;
 
