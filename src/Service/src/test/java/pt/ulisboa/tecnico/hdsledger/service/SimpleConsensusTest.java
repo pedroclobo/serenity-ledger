5275383dbe7c83791f87d2aaf1d5e2c7aad7a02b
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-02 11:21:58 +0000

    test: add multiple append and concurrent append test cases

--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/SimpleConsensusTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/SimpleConsensusTest.java
@@ -1,9 +1,11 @@
 package pt.ulisboa.tecnico.hdsledger.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -51,9 +53,16 @@ class SimpleConsensusTest {
     library = new Library(newNodesConfig, newClientsConfig[0], true);
   }
 
+  @AfterEach
+  public void tearDown() {
+    for (Node node : nodes) {
+      node.shutdown();
+    }
+    library.shutdown();
+  }
+
   @Test
-  // TODO: device when return to the client
-  void proposeSingleValue() {
+  void singleAppend() {
     for (Node node : nodes) {
       assertEquals(0, node.getNodeService().getLedger().size());
     }
@@ -65,4 +74,40 @@ class SimpleConsensusTest {
       assertEquals("value", node.getNodeService().getLedger().get(0));
     }
   }
+
+  @Test
+  public void multipleAppends() {
+    for (Node node : nodes) {
+      assertEquals(0, node.getNodeService().getLedger().size());
+    }
+
+    library.append("value1");
+    library.append("value2");
+    library.append("value3");
+
+    for (Node node : nodes) {
+      assertEquals(3, node.getNodeService().getLedger().size());
+      assertEquals("value1", node.getNodeService().getLedger().get(0));
+      assertEquals("value2", node.getNodeService().getLedger().get(1));
+      assertEquals("value3", node.getNodeService().getLedger().get(2));
+    }
+  }
+
+  @Test
+  public void multipleConcurrentAppends() throws InterruptedException {
+    Thread thread1 = new Thread(() -> library.append("value1"));
+    Thread thread2 = new Thread(() -> library.append("value2"));
+
+    thread1.start();
+    thread2.start();
+    thread1.join();
+    thread2.join();
+
+    for (Node node : nodes) {
+      assertEquals(2, node.getNodeService().getLedger().size());
+      assertTrue(node.getNodeService().getLedger().contains("value1"));
+      assertTrue(node.getNodeService().getLedger().contains("value2"));
+    }
+  }
+
 }
