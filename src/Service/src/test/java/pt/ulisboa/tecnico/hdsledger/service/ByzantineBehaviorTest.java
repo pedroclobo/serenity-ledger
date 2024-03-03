d9907dbf37a3490d15edbc708213cbe19ec0d6a7
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-03 12:01:17 +0000

    refactor: common code in superclass and read config from files

---
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/ByzantineBehaviorTest.java
@@ -0,0 +1,59 @@
+package pt.ulisboa.tecnico.hdsledger.service;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import pt.ulisboa.tecnico.hdsledger.library.Library;
+import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
+import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
+
+abstract class ByzantineBehaviorTest {
+
+  private String nodesConfigPath = "src/main/resources/";
+  private String clientsConfigPath = "../Client/src/main/resources/";
+
+  protected ArrayList<Node> nodes;
+  protected Library library;
+
+  public ByzantineBehaviorTest(String nodeConfigFile, String clientConfigFile) {
+    nodesConfigPath += nodeConfigFile;
+    clientsConfigPath += clientConfigFile;
+  }
+
+  public ProcessConfig[] parseConfigs(String configPath) {
+    return ProcessConfigBuilder.fromFile(configPath);
+  }
+
+  @BeforeEach
+  public final void setUp() {
+    nodes = new ArrayList<>();
+
+    ProcessConfig[] nodesConfig = parseConfigs(nodesConfigPath);
+    ProcessConfig[] clientsConfig = parseConfigs(clientsConfigPath);
+    for (ProcessConfig nodeConfig : nodesConfig) {
+      Node node = new Node(nodeConfig.getId(), nodesConfig, clientsConfig);
+      nodes.add(node);
+      node.start();
+    }
+
+    // Make the library use the client port
+    // A new array has to be created or deep copy the original
+    ProcessConfig[] newNodesConfig = parseConfigs(nodesConfigPath);
+    ProcessConfig[] newClientsConfig = parseConfigs(clientsConfigPath);
+    for (ProcessConfig nodeConfig : newNodesConfig) {
+      nodeConfig.setPort(nodeConfig.getClientPort());
+    }
+    library = new Library(newNodesConfig, newClientsConfig[0], true);
+  }
+
+  @AfterEach
+  public final void tearDown() {
+    for (Node node : nodes) {
+      node.shutdown();
+    }
+    library.shutdown();
+  }
+
+}
