ccd48af206cf05b1c596b1d794fcd38b7dc301e1
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-03 12:01:17 +0000

    refactor: common code in superclass and read config from files

--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/DropByzantineBehaviorTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/DropByzantineBehaviorTest.java
@@ -3,63 +3,14 @@ package pt.ulisboa.tecnico.hdsledger.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import pt.ulisboa.tecnico.hdsledger.library.Library;
-import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
-import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig.ByzantineBehavior;
 
-class DropByzantineBehaviorTest {
+class DropByzantineBehaviorTest extends ByzantineBehaviorTest {
 
-  private String nodesConfigJson = String.join(",\n",
-      "[{ \"id\": \"1\", \"isLeader\": true, \"hostname\": \"localhost\", \"port\": 3001, \"clientPort\": 4001, \"byzantineBehavior\": \"None\"}",
-      "{ \"id\": \"2\", \"isLeader\": false, \"hostname\": \"localhost\", \"port\": 3002, \"clientPort\": 4002, \"byzantineBehavior\": \"Drop\"}",
-      "{ \"id\": \"3\", \"isLeader\": false, \"hostname\": \"localhost\", \"port\": 3003, \"clientPort\": 4003, \"byzantineBehavior\": \"None\"}",
-      "{ \"id\": \"4\", \"isLeader\": false, \"hostname\": \"localhost\", \"port\": 3004, \"clientPort\": 4004, \"byzantineBehavior\": \"None\"}]");
-
-  private String clientsConfigJson =
-      String.join(",\n", "[{ \"id\": \"5\", \"hostname\": \"localhost\", \"port\": 3005 }]");
-
-  private ArrayList<Node> nodes;
-  private Library library;
-
-  private ProcessConfig[] parseConfigs(String nodesConfig) {
-    return ProcessConfigBuilder.fromJson(nodesConfig);
-  }
-
-  @BeforeEach
-  public void setUp() {
-    nodes = new ArrayList<>();
-
-    ProcessConfig[] nodesConfig = parseConfigs(nodesConfigJson);
-    ProcessConfig[] clientsConfig = parseConfigs(clientsConfigJson);
-    for (ProcessConfig nodeConfig : nodesConfig) {
-      Node node = new Node(nodeConfig.getId(), nodesConfig, clientsConfig);
-      nodes.add(node);
-      node.start();
-    }
-
-    // Make the library use the client port
-    // A new array has to be created or deep copy the original
-    ProcessConfig[] newNodesConfig = parseConfigs(nodesConfigJson);
-    ProcessConfig[] newClientsConfig = parseConfigs(clientsConfigJson);
-    for (ProcessConfig nodeConfig : newNodesConfig) {
-      nodeConfig.setPort(nodeConfig.getClientPort());
-    }
-    library = new Library(newNodesConfig, newClientsConfig[0], true);
-  }
-
-  @AfterEach
-  public void tearDown() {
-    for (Node node : nodes) {
-      node.shutdown();
-    }
-    library.shutdown();
+  public DropByzantineBehaviorTest() {
+    super("drop_config.json", "client_config.json");
   }
 
   @Test
