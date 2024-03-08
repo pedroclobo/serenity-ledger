d9db8da8e6edb2b0f1001aba47c9c2c0b7d98b44
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-08 17:06:12 +0000

    test: re-enable concurrent tests

--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/FakeLeaderByzantineBehaviorTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/FakeLeaderByzantineBehaviorTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FakeLeaderByzantineBehaviorTest extends ByzantineBehaviorTest {
@@ -63,7 +62,6 @@ class FakeLeaderByzantineBehaviorTest extends ByzantineBehaviorTest {
   }
 
   @Test
-  @Disabled
   public void multipleConcurrentAppends() throws InterruptedException {
     Thread thread1 = new Thread(() -> library.append("value1"));
     Thread thread2 = new Thread(() -> library.append("value2"));
