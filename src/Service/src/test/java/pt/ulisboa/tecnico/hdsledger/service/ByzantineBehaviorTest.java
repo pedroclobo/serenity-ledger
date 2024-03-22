b7f7b565476d83f7616e4f2e81575ddbd3fef9bb
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-22 14:11:59 +0000

    test: balance request

--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/ByzantineBehaviorTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/ByzantineBehaviorTest.java
@@ -5,17 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pt.ulisboa.tecnico.hdsledger.communication.BalanceResponse;
 import pt.ulisboa.tecnico.hdsledger.library.Library;
+import pt.ulisboa.tecnico.hdsledger.service.models.Block;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
 
 public abstract class ByzantineBehaviorTest {
+
+  private static String keysPath = "../PKI/src/main/resources/keys/";
+  private static int BLOCK_SIZE = 1;
 
   private String nodesConfigPath = "src/main/resources/";
   private String clientsConfigPath = "../Client/src/main/resources/";
@@ -46,7 +50,7 @@ public abstract class ByzantineBehaviorTest {
     ProcessConfig[] nodesConfig = parseConfigs(nodesConfigPath);
     ProcessConfig[] clientsConfig = parseConfigs(clientsConfigPath);
     for (ProcessConfig nodeConfig : nodesConfig) {
-      Node node = new Node(nodeConfig.getId(), nodesConfig, clientsConfig, true);
+      Node node = new Node(nodeConfig.getId(), nodesConfig, clientsConfig, BLOCK_SIZE, false);
       nodes.add(node);
       node.start();
     }
@@ -70,76 +74,25 @@ public abstract class ByzantineBehaviorTest {
   }
 
   @Test
-  void singleAppend() {
+  void checkBalance() {
     for (Node node : nodes) {
       assertEquals(0, node.getNodeService().getLedger().size());
     }
 
-    library.append("value");
+    BalanceResponse response = library.balance(keysPath + "client5.pub");
+    assertEquals(1000, response.getAmount(), "Initial balance should be 1000");
 
     List<Integer> sizes = new ArrayList<>();
-    List<String> values = new ArrayList<>();
+    List<List<Block>> ledgers = new ArrayList<>();
     for (Node node : nodes) {
       sizes.add(node.getNodeService().getLedger().size());
-      values.add(String.join("", node.getNodeService().getLedger()));
+      ledgers.add(node.getNodeService().getLedger());
     }
 
     long sizeCount = sizes.stream().filter(size -> size == 1).count();
     assertTrue(sizeCount >= f + 1, "At least f + 1 nodes should have ledgers of size " + 1);
 
-    long valueCount = values.stream().filter(value -> value.equals("value")).count();
-    assertTrue(valueCount >= f + 1, "At least f + 1 nodes should have the correct ledger");
+    // TODO: compare ledgers
   }
 
-  @Test
-  public void multipleAppends() {
-    for (Node node : nodes) {
-      assertEquals(0, node.getNodeService().getLedger().size());
-    }
-
-    library.append("value1");
-    library.append("value2");
-    library.append("value3");
-
-    List<Integer> sizes = new ArrayList<>();
-    List<String> values = new ArrayList<>();
-    for (Node node : nodes) {
-      sizes.add(node.getNodeService().getLedger().size());
-      values.add(String.join("", node.getNodeService().getLedger()));
-    }
-
-    long sizeCount = sizes.stream().filter(size -> size == 3).count();
-    assertTrue(sizeCount >= f + 1, "At least f + 1 nodes should have ledgers of size " + 3);
-
-    long valueCount = values.stream().filter(value -> value.equals("value1value2value3")).count();
-    assertTrue(valueCount >= f + 1, "At least f + 1 nodes should have the correct ledger");
-  }
-
-  @Test
-  public void multipleConcurrentAppends() throws InterruptedException {
-    Thread thread1 = new Thread(() -> library.append("value1"));
-    Thread thread2 = new Thread(() -> library.append("value2"));
-
-    thread1.start();
-    thread2.start();
-    thread1.join();
-    thread2.join();
-
-    List<Integer> sizes = new ArrayList<>();
-    List<String> values = new ArrayList<>();
-    for (Node node : nodes) {
-      sizes.add(node.getNodeService().getLedger().size());
-      values.add(String.join("", node.getNodeService().getLedger()));
-    }
-
-    long sizeCount = sizes.stream().filter(size -> size == 2).count();
-    assertTrue(sizeCount >= f + 1, "At least f + 1 nodes should have ledgers of size " + 2);
-
-    List<Long> valueCount =
-        values.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting())).values()
-            .stream().collect(Collectors.toList());
-
-    assertTrue(valueCount.stream().anyMatch(count -> count >= f + 1),
-        "At least f + 1 nodes should have the correct ledger");
-  }
 }
