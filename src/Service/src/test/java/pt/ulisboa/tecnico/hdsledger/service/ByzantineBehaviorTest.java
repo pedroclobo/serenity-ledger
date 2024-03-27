b19c385f2928d84bba672aac324000cad6243298
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2024-03-27 17:56:37 +0000

    test: seperate config files and tests

--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/ByzantineBehaviorTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/ByzantineBehaviorTest.java
@@ -1,23 +1,14 @@
 package pt.ulisboa.tecnico.hdsledger.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import pt.ulisboa.tecnico.hdsledger.communication.application.BalanceRequest;
-import pt.ulisboa.tecnico.hdsledger.communication.application.BalanceResponse;
-import pt.ulisboa.tecnico.hdsledger.communication.application.TransferResponse;
 import pt.ulisboa.tecnico.hdsledger.library.Library;
-import pt.ulisboa.tecnico.hdsledger.service.models.Ledger;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfig;
 import pt.ulisboa.tecnico.hdsledger.utilities.ProcessConfigBuilder;
-import pt.ulisboa.tecnico.hdsledger.utilities.RSACryptography;
 
 public abstract class ByzantineBehaviorTest {
 
@@ -82,44 +73,4 @@ public abstract class ByzantineBehaviorTest {
     }
   }
 
-  @Test
-  void transfersAndBalances() {
-    for (Node node : nodes) {
-      assertEquals(0, node.getNodeService().getLedger().getLedger().size());
-    }
-
-    BalanceResponse balanceResponse = libraries.get(0).balance(5);
-    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
-    assertEquals(1000, balanceResponse.getAmount().get(), "Initial balance should be 1000");
-
-    balanceResponse = libraries.get(0).balance(6);
-    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
-    assertEquals(1000, balanceResponse.getAmount().get(), "Initial balance should be 1000");
-
-    TransferResponse transferResponse = libraries.get(0).transfer(5, 6, 100);
-    assertTrue(transferResponse.isSuccessful(), "Transfer request should be successful");
-
-    balanceResponse = libraries.get(0).balance(5);
-    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
-    assertEquals(900, balanceResponse.getAmount().get(), "New balance should be 900");
-
-    balanceResponse = libraries.get(0).balance(6);
-    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
-    assertEquals(1100, balanceResponse.getAmount().get(), "New balance should be 1100");
-
-    List<Integer> sizes = new ArrayList<>();
-    List<Ledger> ledgers = new ArrayList<>();
-    for (Node node : nodes) {
-      Ledger ledger = node.getNodeService().getLedger();
-      sizes.add(ledger.getLedger().size());
-      ledgers.add(ledger);
-    }
-
-    long sizeCount = sizes.stream().filter(size -> size == 5).count();
-    assertTrue(sizeCount >= f + 1, "At least f + 1 nodes should have ledgers of size " + 5);
-
-    List<Ledger> ledgersWithSizeFPlus1 =
-        ledgers.stream().filter(ledger -> ledger.getLedger().size() == 5).toList();
-    assertEquals(1, ledgersWithSizeFPlus1.stream().distinct().count(), "Ledgers should be equal");
-  }
 }
--- src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/fake_value/FakeValueByzantineBehaviorTest.java
+++ src/Service/src/test/java/pt/ulisboa/tecnico/hdsledger/service/fake_value/FakeValueByzantineBehaviorTest.java
@@ -1,11 +1,64 @@
 package pt.ulisboa.tecnico.hdsledger.service.fake_value;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import pt.ulisboa.tecnico.hdsledger.communication.application.BalanceResponse;
+import pt.ulisboa.tecnico.hdsledger.communication.application.TransferResponse;
 import pt.ulisboa.tecnico.hdsledger.service.ByzantineBehaviorTest;
+import pt.ulisboa.tecnico.hdsledger.service.Node;
+import pt.ulisboa.tecnico.hdsledger.service.models.Ledger;
 
 public class FakeValueByzantineBehaviorTest extends ByzantineBehaviorTest {
 
   public FakeValueByzantineBehaviorTest() {
-    super("fake_value/fake_value.json", "client_config.json");
+    super("fake_value/nodes.json", "fake_value/clients.json");
+  }
+
+  @Test
+  void transfersAndBalances() {
+    for (Node node : nodes) {
+      assertEquals(0, node.getNodeService().getLedger().getLedger().size());
+    }
+
+    BalanceResponse balanceResponse = libraries.get(0).balance(5);
+    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
+    assertEquals(1000, balanceResponse.getAmount().get(), "Initial balance should be 1000");
+
+    balanceResponse = libraries.get(0).balance(6);
+    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
+    assertEquals(1000, balanceResponse.getAmount().get(), "Initial balance should be 1000");
+
+    TransferResponse transferResponse = libraries.get(0).transfer(5, 6, 100);
+    assertTrue(transferResponse.isSuccessful(), "Transfer request should be successful");
+
+    balanceResponse = libraries.get(0).balance(5);
+    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
+    assertEquals(900, balanceResponse.getAmount().get(), "New balance should be 900");
+
+    balanceResponse = libraries.get(0).balance(6);
+    assertTrue(balanceResponse.isSuccessful(), "Balance request should be successful");
+    assertEquals(1100, balanceResponse.getAmount().get(), "New balance should be 1100");
+
+    List<Integer> sizes = new ArrayList<>();
+    List<Ledger> ledgers = new ArrayList<>();
+    for (Node node : nodes) {
+      Ledger ledger = node.getNodeService().getLedger();
+      sizes.add(ledger.getLedger().size());
+      ledgers.add(ledger);
+    }
+
+    long sizeCount = sizes.stream().filter(size -> size == 5).count();
+    assertTrue(sizeCount >= f + 1, "At least f + 1 nodes should have ledgers of size " + 5);
+
+    List<Ledger> ledgersWithSizeFPlus1 =
+        ledgers.stream().filter(ledger -> ledger.getLedger().size() == 5).toList();
+    assertEquals(1, ledgersWithSizeFPlus1.stream().distinct().count(), "Ledgers should be equal");
   }
 
 }
